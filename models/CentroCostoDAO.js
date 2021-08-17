c16448e6e712d37ae2406dd0ba4547f8769035a1
Luis-Donato-Martinez-Simuta <74307313+Luis-Donato-Martinez-Simuta@users.noreply.github.com>
2021-08-17 14:52:23 -0500

    Add files via upload

--- models/CentroCostoDAO.js
+++ models/CentroCostoDAO.js
@@ -17,7 +17,25 @@ function obtenerTodasUnidades(callback) {
     });
 }
 
+function obtenerCentroCostoID(IdCentroCosto,callback) {
+    console.log("id centro costo uno: "+ IdCentroCosto)
+
+    let sql = "call obtenerCentroCostoID("+IdCentroCosto+");";
+
+    db.query(sql, (err, data) => {
+        if (err) {
+            throw err
+        };
+        if (data.length > 0) {
+            return callback(data[0][0]);
+        };
+
+        return callback(null);
+    });
+}
+
 module.exports = {
-    obtenerTodasUnidades
+    obtenerTodasUnidades,
+    obtenerCentroCostoID
 }
 
--- routes/index.js
+++ routes/index.js
@@ -12,102 +12,77 @@ router.get('/', function (req, res, next) {
   res.render('login');
 });
 
+//Si se quiere entrar al sistema con el link primero le pedira que se logue
 router.get('/*', function (req, res, next) {
   res.render('login');
 });
 
-
+//Manda a llamar el login cuando el usuarios quiere salir del sistema
 router.get('/irLogin', function (req, res, next) {
   res.render('login');
 });
 
-router.post('/irMain', function (req, res, next) {
-  let {
-    Id
-  } = req.body;
-
-  UsuarioDAO.obtenerUsuarioPorId(Id, (data) => {
-    usuario = data;
-    CCDAO.obtenerTodasUnidades((data) => {
-      listaUsuarios = data;
-
-      res.render('main', {
-        listaUsuarios: listaUsuarios,
-        usuario: usuario
-      });
-    });
-  });
+//Se manda a llamar cuando la vista esta en construccion
+router.post('/enConstruccion', function (req, res, next) {
+  res.render('construccion');
 });
 
 
-
-router.post('/verListaUsuarios', function (req, res, next) {
-  let {
-    Id
-  } = req.body;
-
-  UsuarioDAO.obtenerUsuarioPorId(Id, (data) => {
-    usuario = data;
-    UsuarioDAO.obtenerTodosUsuarios((data) => {
-      listaUsuarios = data;
-      res.render('verUsuarios', {
-        listaUsuarios: listaUsuarios,
-        usuario: usuario
-      });
-    });
-  });
-});
-
+//Madnamos a llamar la pantalla de los centros de costo
 router.post('/verListaCentrosCosto', function (req, res, next) {
+  //Recuperamos el Id del usuario en la pantalla
   let {
-    Id
+    IdUsuario
   } = req.body;
-
-  UsuarioDAO.obtenerUsuarioPorId(Id, (data) => {
-    usuario = data;
-    UsuarioDAO.obtenerTodosUsuarios((data) => {
-      listaFranquicias = data;
-
-      res.render('main', {
-        listaFranquicias: listaFranquicias,
+  
+  //Obtenemos el Usuario para tener de referencia en la pantalla
+  UsuarioDAO.obtenerUsuarioPorId(IdUsuario, (data) => {
+    let usuario = data;
+    console.log(usuario)
+    //Obtenemos todos los centros de cosostos
+    CCDAO.obtenerTodasUnidades((data)=>{
+      listaCentrosCosotos = data;
+      //Rendirizamos la pantalla de lista de centros de costo
+      res.render('administracion/listas/listaCentroCostos', {
+        listaCentrosCosotos: listaCentrosCosotos,
         usuario: usuario
       });
     });
   });
 });
 
-router.post('/verListaFranquicias', function (req, res, next) {
+router.post('/verCentroCosto', function (req, res, next) {
   let {
-    Id
+    IdCentroCosto
   } = req.body;
 
-  UsuarioDAO.obtenerUsuarioPorId(Id, (data) => {
-    usuario = data;
+  console.log("id Centro: "+IdCentroCosto)
+  CCDAO.obtenerCentroCostoID(IdCentroCosto, (data) => {
+    centroCosto = data;
    
-    franquiciaDAO.obtenerTodasFranquicias((data) =>{
-      listaFranquicia = data;
-
-      res.render('verFranquicias', {
-        listaFranquicia: listaFranquicia,
-        usuario: usuario
-      });
+    console.log("centro costo:", centroCosto)
+    res.render('verCentroCostoID', {
+      centroCosto: centroCosto
     });
+    
   });
 });
 
-router.post('/verListaEmpresas', function (req, res, next) {
-  let {
-    Id
-  } = req.body;
-  
 
-  
-  UsuarioDAO.obtenerUsuarioPorId(Id, (data) => {
+//Madnamos a llamar la pantalla de las empresas
+router.post('/verListaEmpresas', function (req, res, next) {
+  //Recuperamos el Id del usuario en la pantalla
+  let {
+    IdUsuario
+  } = req.body;
+  //Obtenemos el Usuario para tener de referencia en la pantalla    
+  UsuarioDAO.obtenerUsuarioPorId(IdUsuario, (data) => {
     usuario = data;
+    //Obtenemos todas las empresas
     empresaDAO.obtenerTodasEmpresas((data) => {
       listaEmpresas = data;
-      
-      res.render('verEmpresas', {
+      //Rendirizamos la pantalla de lista de empresas
+      res.render('administracion/listas/listaEmpresas', {
         listaEmpresas: listaEmpresas,
         usuario: usuario
       });
@@ -115,19 +90,64 @@ router.post('/verListaEmpresas', function (req, res, next) {
   });
 });
 
-router.post('/verListaTipoUnidad', function (req, res, next) {
-  let {
-    Id
-  } = req.body;
-  
 
-  
-  UsuarioDAO.obtenerUsuarioPorId(Id, (data) => {
+//Madnamos a llamar la pantalla de las franquicias
+router.post('/verListaFranquicias', function (req, res, next) {
+  //Recuperamos el Id del usuario en la pantalla
+  let {
+    IdUsuario
+  } = req.body;
+  //Obtenemos el Usuario para tener de referencia en la pantalla
+  UsuarioDAO.obtenerUsuarioPorId(IdUsuario, (data) => {
     usuario = data;
+    //Obtenemos todas las franquicias    
+    franquiciaDAO.obtenerTodasFranquicias((data) =>{
+      listaFranquicia = data;
+      //Rendirizamos la pantalla de lista de franquicias
+      res.render('administracion/listas/listaFranquicias', {
+        listaFranquicia: listaFranquicia,
+        usuario: usuario
+      });
+    });
+  });
+});
+
+//Madnamos a llamar la pantalla de tipo unidad
+router.post('/verListaTipoUnidad', function (req, res, next) {
+  //Recuperamos el Id del usuario en la pantalla
+  let {
+    IdUsuario
+  } = req.body;
+  //Obtenemos el Usuario para tener de referencia en la pantalla
+  UsuarioDAO.obtenerUsuarioPorId(IdUsuario, (data) => {
+    usuario = data;
+    //Obtenemos todas los tipos de unidad 
     tipoUnidad.obtenerTodosTipoUnidad((data) => {
       listaTipoUnidad = data;
-      res.render('verTipoUnidad', {
+      //Rendirizamos la pantalla de lista de tipos de unidad
+      res.render('administracion/listas/listaTipoUnidades', {
         listaTipoUnidad: listaTipoUnidad,
+        usuario: usuario
+      });
+    });
+  });
+});
+
+//Madnamos a llamar la pantalla de lista de usuarios
+router.post('/verListaUsuarios', function (req, res, next) {
+  //Recuperamos el Id del usuario en la pantalla
+  let {
+    IdUsuario
+  } = req.body;
+  //Obtenemos el Usuario para tener de referencia en la pantalla
+  UsuarioDAO.obtenerUsuarioPorId(IdUsuario, (data) => {
+    usuario = data;
+    //Obtenemos todas los usuarios
+    UsuarioDAO.obtenerTodosUsuarios((data) => {
+      listaUsuarios = data;
+      //Rendirizamos la pantalla de lista de lista de usuarios
+      res.render('administracion/listas/listaUsuarios', {
+        listaUsuarios: listaUsuarios,
         usuario: usuario
       });
     });
@@ -136,31 +156,40 @@ router.post('/verListaTipoUnidad', function (req, res, next) {
 
 //Fucion que permite hacer el laamado de logue del usuario
 router.post('/logueo', async function (req, res, next) {
-
+  //Obtenemos el nombre de usuario y la contraseña para buscarlo en el sistema
   let {
-    username,
-    password
+    username /*Nombre del usuario*/,
+    password /*Contraseña del usuario*/
   } = req.body;
-
+  //Incriptamos la contraseña del ususairo
   let passwordincriptado = md5(password);
-
+  //Funcion que manda a llamar el logue en la base de datos
   UsuarioDAO.logueo(username, passwordincriptado, (data) => {
     usuario = data;
-
+    console.log(usuario);
+    //Valida si el usuario existe en la base de datos
     if (usuario == null) {
+      //Si el usuario no existe se regresa al login con un mensaje indicando
+      //Acceso denegado
       res.render('login', {
         acceso: false
       });
     } else {
+      //Si el usuario existe te manda a la pagina principal mostrando
+      //Todos los centros de costo
       CCDAO.obtenerTodasUnidades((data) => {
-        centroCosto = data;
-        res.render('main', {
-          centroCosto: centroCosto,
+        listaCentrosCosotos = data;
+        res.render('administracion/listas/listaCentroCostos', {
+          listaCentrosCosotos: listaCentrosCosotos,
           usuario: usuario
         });
       });
     }
   });
+});
+
+router.post("/miPerfil", function (req, res, next){
+  res.render('miPerfil');
 });
 
 module.exports = router;
